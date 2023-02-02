6bf6a1d8439eb7eaf1ed2cfd940aa4d487e30299
ZoeFA <d22125460@mytudublin.ie>
2023-02-02 09:08:45 +0000

    lab2

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,63 +5,77 @@ import processing.core.PApplet;
 public class BugZap extends PApplet
 {
 
+	float playerX, playerY, playerWidth;
+	//float x1dir, x2dir, y1dir, y2dir;
+	//float c = 0;
+
 	public void settings()
 	{
 		size(500, 500);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		//colorMode(HSB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
-		
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+	public void drawPlayer(float x, float y, float w){
+		
+		background(0);
+		stroke(0, 0, 255);
+		fill(0, 0, 255);
+		circle(x, y, w);
+
+	}
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
-
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
 		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
+		drawPlayer(playerX, playerY, 100);
+		
+		
+	}
+
+	public void keyPressed(){
+		
+
+		if (keyCode == LEFT){
+
+			if (playerX > 0){
+			//System.out.println("Left arrow pressed");
+			playerX--;
+			}	
 		}
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
+		if (keyCode == RIGHT){
+
+			if (playerX < width){
+			//System.out.println("Right arrow pressed");
+			playerX++;
+			}
 		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
+
+
+		if (keyCode == UP){
+
+			if (playerY < height){
+			//System.out.println("Left arrow pressed");
+			playerX--;
+			}
+		}
+
+		if (keyCode == DOWN){
+
+			if (playerX < width){
+			//System.out.println("Right arrow pressed");
+			playerX++;
+			}
+		}
+
+		if (key == ' '){
+			//System.out.println("SPACE key pressed");
+			line(playerX, playerY, width/10, width/5);
 		}
 	}
+
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,12 +3,12 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
+	/*public static void helloProcessing()
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
-
+	*/
 	public static void bugZap()
 	{
 		String[] a = {"MAIN"};
