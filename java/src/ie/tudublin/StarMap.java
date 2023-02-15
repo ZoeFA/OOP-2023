79386d20cc018d0ca83d23a05eb5799c4a0f134b
ZoeFA <d22125460@mytudublin.ie>
2023-02-15 16:44:41 +0000

    lab4

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -25,9 +25,10 @@ public class StarMap extends PApplet
 		float border = 50.0f;
 		
 		int count = 10;
-		float gap = width - (border * 2.0f) / (float) count;
+		float gap = (width - (border * 2.0f))/ (float) count;
 
 		for (int i = -5; i <= 5; i++){
+
 			float x = border + (gap * (i + 5));
 			line(x, border, x, height - border);
 			line(border, x, width - border, x);
