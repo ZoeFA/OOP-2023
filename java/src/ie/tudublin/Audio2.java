9c94c244bd59c68595fa26fcdda26965c5887ec0
ZoeFA <d22125460@mytudublin.ie>
2023-02-27 12:01:05 +0000

    code

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -28,27 +28,41 @@ public class Audio2 extends PApplet
     {
         m = new Minim(this);
         ai = m.getLineIn(Minim.MONO, width, 44100, 16);
-        ap -= ai.mix;
+        ap = ai.mix;
+        lerpedBuffer = new float[width];
     }
-
-    float off = 0;
 
     float lerpedBuffer[] = new float[1024];
 
     public void draw()
     {
         background(0);
+        colorMode(HSB);
         stroke(255);
 
         float half = height / 2;
-        
+
         for(int i = 0; i < ab.size(); i ++){
 
-          line(i, half, 1, half + ab.get(i) * half);
+          stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
+          lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
+          float f = abs(lerpedBuffer[i] * half * 2.0f);
+          line(i, half + f, i, half - f);
         }
 
+        //println(map(5, 0, 10, 1000, 2000));
+        //println(map1(5, 2, 10, 1000, 2000));
 
            
     }
 
-}        
+    float map1(float a, float b, float c, float d, float e){
+
+      float range1 = c - b;
+      float range2 = e - d;
+      float howFar = a - b;
+      
+      return d + (howFar / range1) * range2;
+    }
+
+}      
