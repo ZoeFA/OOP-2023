24c3330c48550e50b1945c0f8b113be59def7196
ZoeFA <d22125460@mytudublin.ie>
2023-04-24 12:13:08 +0100

    i hate everything

--- java/src/ie/tudublin/Bullet.java
+++ java/src/ie/tudublin/Bullet.java
@@ -72,16 +72,6 @@ public class Bullet {
             ((YASC)p).bullets.remove(this);
         }
 
-<<<<<<< HEAD
-        if (pos.x <0){
-            pos.x = p.width;
-
-        }
-        //if(pos.y )
-
-
-=======
->>>>>>> d1af8cf42076566858f7fc2350fd9581150f2cd1
     }
 
 
